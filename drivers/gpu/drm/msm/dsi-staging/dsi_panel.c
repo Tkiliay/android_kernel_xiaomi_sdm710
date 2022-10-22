27982ff409ba0eafdd7c621568dbf54336eb957b
Tkiliay <sackclouths@outlook.com>
2022-10-22 18:31:06 +0800

    Revert "drm: make OLED great again"
    This reverts commit 345b675a4a673ebd33305e082b99d50b92750545.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -23,10 +23,6 @@
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
 
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
-#include "exposure_adjustment.h"
-#endif
-
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -931,10 +927,6 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 
 	if (panel->type == EXT_BRIDGE)
 		return 0;
-
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
-	bl_lvl = ea_panel_calc_backlight(bl_lvl);
-#endif
 
 	pr_debug("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
 	switch (bl->type) {
