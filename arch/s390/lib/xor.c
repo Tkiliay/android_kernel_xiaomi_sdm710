6dba54fa4898f3ebcc7faa71e4e493edb26fafda
Tkiliay <sackclouths@outlook.com>
2023-09-02 21:47:53 +0800

    Revert "lib/xor: make xor prototypes more friendly to compiler vectorization"
    This reverts commit 4f56a231000a3c39426870fdb1930fad1e48b751.

--- arch/s390/lib/xor.c
+++ arch/s390/lib/xor.c
@@ -9,8 +9,7 @@
 #include <linux/module.h>
 #include <linux/raid/xor.h>
 
-static void xor_xc_2(unsigned long bytes, unsigned long * __restrict p1,
-		     const unsigned long * __restrict p2)
+static void xor_xc_2(unsigned long bytes, unsigned long *p1, unsigned long *p2)
 {
 	asm volatile(
 		"	larl	1,2f\n"
@@ -31,9 +30,8 @@ static void xor_xc_2(unsigned long bytes, unsigned long * __restrict p1,
 		: "0", "1", "cc", "memory");
 }
 
-static void xor_xc_3(unsigned long bytes, unsigned long * __restrict p1,
-		     const unsigned long * __restrict p2,
-		     const unsigned long * __restrict p3)
+static void xor_xc_3(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+		     unsigned long *p3)
 {
 	asm volatile(
 		"	larl	1,2f\n"
@@ -58,10 +56,8 @@ static void xor_xc_3(unsigned long bytes, unsigned long * __restrict p1,
 		: : "0", "1", "cc", "memory");
 }
 
-static void xor_xc_4(unsigned long bytes, unsigned long * __restrict p1,
-		     const unsigned long * __restrict p2,
-		     const unsigned long * __restrict p3,
-		     const unsigned long * __restrict p4)
+static void xor_xc_4(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+		     unsigned long *p3, unsigned long *p4)
 {
 	asm volatile(
 		"	larl	1,2f\n"
@@ -90,11 +86,8 @@ static void xor_xc_4(unsigned long bytes, unsigned long * __restrict p1,
 		: : "0", "1", "cc", "memory");
 }
 
-static void xor_xc_5(unsigned long bytes, unsigned long * __restrict p1,
-		     const unsigned long * __restrict p2,
-		     const unsigned long * __restrict p3,
-		     const unsigned long * __restrict p4,
-		     const unsigned long * __restrict p5)
+static void xor_xc_5(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+		     unsigned long *p3, unsigned long *p4, unsigned long *p5)
 {
 	/* Get around a gcc oddity */
 	register unsigned long *reg7 asm ("7") = p5;
--- arch/x86/include/asm/xor.h
+++ arch/x86/include/asm/xor.h
@@ -68,8 +68,7 @@
 					op(i + 3, 3)
 
 static void
-xor_sse_2(unsigned long bytes, unsigned long * __restrict p1,
-	  const unsigned long * __restrict p2)
+xor_sse_2(unsigned long bytes, unsigned long *p1, unsigned long *p2)
 {
 	unsigned long lines = bytes >> 8;
 
@@ -120,8 +119,7 @@ xor_sse_2(unsigned long bytes, unsigned long * __restrict p1,
 }
 
 static void
-xor_sse_2_pf64(unsigned long bytes, unsigned long * __restrict p1,
-	       const unsigned long * __restrict p2)
+xor_sse_2_pf64(unsigned long bytes, unsigned long *p1, unsigned long *p2)
 {
 	unsigned long lines = bytes >> 8;
 
@@ -155,9 +153,8 @@ xor_sse_2_pf64(unsigned long bytes, unsigned long * __restrict p1,
 }
 
 static void
-xor_sse_3(unsigned long bytes, unsigned long * __restrict p1,
-	  const unsigned long * __restrict p2,
-	  const unsigned long * __restrict p3)
+xor_sse_3(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	  unsigned long *p3)
 {
 	unsigned long lines = bytes >> 8;
 
@@ -215,9 +212,8 @@ xor_sse_3(unsigned long bytes, unsigned long * __restrict p1,
 }
 
 static void
-xor_sse_3_pf64(unsigned long bytes, unsigned long * __restrict p1,
-	       const unsigned long * __restrict p2,
-	       const unsigned long * __restrict p3)
+xor_sse_3_pf64(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	       unsigned long *p3)
 {
 	unsigned long lines = bytes >> 8;
 
@@ -253,10 +249,8 @@ xor_sse_3_pf64(unsigned long bytes, unsigned long * __restrict p1,
 }
 
 static void
-xor_sse_4(unsigned long bytes, unsigned long * __restrict p1,
-	  const unsigned long * __restrict p2,
-	  const unsigned long * __restrict p3,
-	  const unsigned long * __restrict p4)
+xor_sse_4(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	  unsigned long *p3, unsigned long *p4)
 {
 	unsigned long lines = bytes >> 8;
 
@@ -321,10 +315,8 @@ xor_sse_4(unsigned long bytes, unsigned long * __restrict p1,
 }
 
 static void
-xor_sse_4_pf64(unsigned long bytes, unsigned long * __restrict p1,
-	       const unsigned long * __restrict p2,
-	       const unsigned long * __restrict p3,
-	       const unsigned long * __restrict p4)
+xor_sse_4_pf64(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	       unsigned long *p3, unsigned long *p4)
 {
 	unsigned long lines = bytes >> 8;
 
@@ -362,11 +354,8 @@ xor_sse_4_pf64(unsigned long bytes, unsigned long * __restrict p1,
 }
 
 static void
-xor_sse_5(unsigned long bytes, unsigned long * __restrict p1,
-	  const unsigned long * __restrict p2,
-	  const unsigned long * __restrict p3,
-	  const unsigned long * __restrict p4,
-	  const unsigned long * __restrict p5)
+xor_sse_5(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	  unsigned long *p3, unsigned long *p4, unsigned long *p5)
 {
 	unsigned long lines = bytes >> 8;
 
@@ -438,11 +427,8 @@ xor_sse_5(unsigned long bytes, unsigned long * __restrict p1,
 }
 
 static void
-xor_sse_5_pf64(unsigned long bytes, unsigned long * __restrict p1,
-	       const unsigned long * __restrict p2,
-	       const unsigned long * __restrict p3,
-	       const unsigned long * __restrict p4,
-	       const unsigned long * __restrict p5)
+xor_sse_5_pf64(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	       unsigned long *p3, unsigned long *p4, unsigned long *p5)
 {
 	unsigned long lines = bytes >> 8;
 
