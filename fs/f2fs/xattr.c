9abe4c83f29ed442e60a683ca1934912a26516d0
Chao Yu <chao@kernel.org>
2024-10-26 18:06:37 +0800

    f2fs: remove unneeded check condition in __f2fs_setxattr()
    It has checked return value of write_all_xattrs(), remove unneeded
    following check condition.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Change-Id: I87d10fa0c2dfa0333177c908ae52ff4ca503b334

--- fs/f2fs/xattr.c
+++ fs/f2fs/xattr.c
@@ -757,7 +757,7 @@ retry:
 			!strcmp(name, F2FS_XATTR_NAME_ENCRYPTION_CONTEXT))
 		f2fs_set_encrypted_inode(inode);
 	f2fs_mark_inode_dirty_sync(inode, true);
-	if (!error && S_ISDIR(inode->i_mode))
+	if (S_ISDIR(inode->i_mode))
 		set_sbi_flag(F2FS_I_SB(inode), SBI_NEED_CP);
 exit:
 	kvfree(base_addr);
